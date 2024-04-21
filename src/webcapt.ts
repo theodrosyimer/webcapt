ff9c3ab6900663137426008e6ea51010060ad647
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-04-21 15:55:04 +0200

    feat: add format handling and better description

--- src/webcapt.ts
+++ src/webcapt.ts
@@ -8,18 +8,32 @@ export const webcapt = new Command()
 webcapt
   .name('webcapt')
   .version('0.0.1')
-  .description('Download webpages as images or pdfs')
+  .description(
+    'A simple cli to screen capture web pages and save them to disk as images or pdfs.',
+  )
 
 webcapt
+  .command('pdf')
   .option('-u, --url <url>', 'URL to download') // https://www.udemy.com/
   .option('-o, --output <output>', 'Output file name')
-  .option('-t, --type <type>', 'Type of file to download, default: pdf', 'pdf')
-  .action((options: { url: string; output: string; type: 'pdf' | 'img' }) => {
-    if (options.type === 'pdf') {
-      downloadPDF(options.url, options.output)
-    }
+  .option('-f, --format <type>', 'Type of file to download, default: A4', 'A4')
+  .action(
+    (options: { url: string; output: string; format: 'A4' | 'letter' }) => {
+      downloadPDF(options.url, options.output, options.format)
+    },
+  )
 
-    if (options.type === 'img') {
-      downloadImg(options.url, options.output)
-    }
-  })
+webcapt
+  .command('img')
+  .option('-u, --url <url>', 'URL to download') // https://www.udemy.com/
+  .option('-o, --output <output>', 'Output file name')
+  .option(
+    '-f, --format <type>',
+    'Type of file to download could be png or jpeg, default: png',
+    'png',
+  )
+  .action(
+    (options: { url: string; output: string; format: 'png' | 'jpeg' }) => {
+      downloadImg(options.url, options.output, options.format)
+    },
+  )
