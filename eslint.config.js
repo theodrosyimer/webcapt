9c55b3195e6a313eafed3aa43b2b75581f022dd0
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2025-08-17 18:50:38 +0200

    chore: migrate ESLint configuration

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import baseConfig from "@thyi/eslint-config"
+
+export default {
+  ...baseConfig,
+
+    languageOptions: {
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname,
+        project: ['./tsconfig.eslint.json'],
+      },
+    },
+    rules: {
+      'new-line-per-chained-call': 'off',
+      'import/no-extraneous-dependencies': [
+        'error',
+        {
+          devDependencies: true,
+        },
+      ],
+  },
+}
