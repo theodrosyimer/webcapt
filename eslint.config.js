e219cdd6c88b85c887be8523092d95b8b049df37
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2026-04-25 20:33:23 +0200

    fix: remove languageOptions from ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,6 @@ import baseConfig from '@thyi/eslint-config'
 export default [
   ...baseConfig,
   {
-    languageOptions: {
-      parserOptions: {
-        tsconfigRootDir: import.meta.dirname,
-        project: ['./tsconfig.json'],
-      },
-    },
     rules: {
       'new-line-per-chained-call': 'off',
       'import/no-extraneous-dependencies': [
