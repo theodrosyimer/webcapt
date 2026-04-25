2db7c991c154886762a5c182f03acfed7f08327d
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2026-04-25 19:42:54 +0200

    fix(generateImage): set default screenshot path to 'screenshot.png'

--- src/lib/generate/image.ts
+++ src/lib/generate/image.ts
@@ -29,7 +29,7 @@ export async function generateImage({
 
     screenshotOptions.path = screenshotOptions.path
       ? (`${screenshotOptions.path}.${screenshotOptions.type}` satisfies ScreenshotOptions['path'])
-      : undefined
+      : 'screenshot.png'
 
     await page.screenshot({
       ...defaultScreenshotOptions,
